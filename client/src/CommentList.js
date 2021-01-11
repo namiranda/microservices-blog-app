aedbc4795c0d9197386988961eae4cc68cd7099e
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-11 16:15:59 -0300

    agrego status de los comment a react-app

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,8 +3,19 @@ import React from "react";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    console.log("comment: " + comment.content);
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return (
