c5b9be71ca0be0f369d7c9e14d4c9863f8bbb073
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-10 21:15:42 -0300

    uso el query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     console.log("comment: " + comment.content);
     return <li key={comment.id}>{comment.content}</li>;
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,7 +8,7 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
 
     setPosts(res.data);
   };
@@ -26,7 +26,7 @@ export default () => {
       >
         <div className="card-body">
           <h2>{post.title}</h2>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
