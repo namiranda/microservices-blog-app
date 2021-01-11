d374d4ff5dc303a2e950b010fea21e591cdb20b1
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-11 15:48:44 -0300

    agrego moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,6 +12,7 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4000/events", event); //posts
   axios.post("http://localhost:4001/events", event); //coments
   axios.post("http://localhost:4002/events", event); //query service
+  axios.post("http://localhost:4003/events", event); //moderation service
 
   res.send({ status: "OK" });
 });
