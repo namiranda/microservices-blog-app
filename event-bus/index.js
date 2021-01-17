6c833304586b133dc8c9ccd0bc38e54da29d190e
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-17 17:04:28 -0300

    agrego route paths y arreglo cosas

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const axios = require("axios");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
-
 app.use(bodyParser.json());
 
 const events = [];
@@ -13,7 +12,7 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://post-clusterip-srv:4000/events", event);
+  axios.post("http://posts-clusterip-srv:4000/events", event);
   axios.post("http://comments-srv:4001/events", event);
   axios.post("http://query-srv:4002/events", event);
   axios.post("http://moderation-srv:4003/events", event);
